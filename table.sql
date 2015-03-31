87f7c4a0e72405edc9755476c79ba8aa376b332d
root <root@berry-love.tk>
2015-03-31 14:57:58 +0900

    Fixed sql

--- table.sql
+++ table.sql
@@ -16,6 +16,6 @@ CREATE TABLE notes (
 -- Dumping data for table `notes`
 --
 
-INSERT INTO notes (text, name, color, xyz) VALUES(1, 'This is the first note! Add yours from the button above.', 'StickyNotes', 'yellow', '478x0x1');
-INSERT INTO notes (text, name, color, xyz) VALUES(2, 'The position of the notes is saved with AJAX.', 'StickyNotes', 'blue', '0x321x2');
-INSERT INTO notes (text, name, color, xyz) VALUES(3, 'The notes are automatically deleted after an hour.', 'StickyNotes', 'green', '311x41x3');
+INSERT INTO notes (id, text, name, color, xyz) VALUES(1, 'This is the first note! Add yours from the button above.', 'StickyNotes', 'yellow', '478x0x1');
+INSERT INTO notes (id, text, name, color, xyz) VALUES(2, 'The position of the notes is saved with AJAX.', 'StickyNotes', 'blue', '0x321x2');
+INSERT INTO notes (id, text, name, color, xyz) VALUES(3, 'The notes are automatically deleted after an hour.', 'StickyNotes', 'green', '311x41x3');
