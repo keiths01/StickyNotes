d5dbf611b8453418ddfab4e91d818dba36435825
keiths01 <99104423+keiths01@users.noreply.github.com>
2022-02-05 17:03:37 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -11,11 +11,12 @@ $password = "pass";
 $hint = "type 'pass'";
 
 /* Database config */
-$db_host	= '';
-$db_user	= '';
+$db_host	= 'localhost';
+$db_user	= 'root';
 $db_pass	= '';
-$db_database	= 'stickynotes.sqlite';
+$db_database	= 'sticky_notes';
 
 /* End config */
-$db = new PDO("sqlite:".$db_database) or die('Unable to establish a DB connection');
+// $db = new PDO("sqlite:".$db_database) or die('Unable to establish a DB connection');
+$db = new PDO('mysql:dbname=sticky_notes;host='.$db_host,$db_user,$db_pass) or die('Unable to establish a DB connection');
 ?>
